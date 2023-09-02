f2bc50b457eecf163f6e9ef937823a16ad1a56d1
ValentinBustamante <140023829+ValentinBustamante@users.noreply.github.com>
2023-09-01 22:31:07 -0300

    Trabajo-11

---
+++ script.js
@@ -0,0 +1,18 @@
+let nombre ="valentin"; 
+let apellido = "bustamante";
+console.log(nombre+" "+apellido);
+
+let solicitud = prompt("¡Hola! Introduce tu nombre");
+
+if (solicitud === "") {
+    prompt("Por favor, introduce un nombre.");
+} else {
+    alert("Hola " + solicitud + ". ¡Bienvenido a la página POP84 Jeans!");
+}
+
+const compra = document.getElementById("compra");
+
+compra.addEventListener("click", function() {
+    alert("Gracias por su compra");
+});
+
